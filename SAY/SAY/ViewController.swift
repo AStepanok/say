b46150d1f2293caf417db38628cb1eb893c37547
Антон Степанок <antonstepanok@MacBook-Pro-Anton-2.local>
2020-03-14 16:46:57 +0300

    xcode

---
+++ SAY/SAY/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SAY
+//
+//  Created by Антон Степанок on 14.03.2020.
+//  Copyright © 2020 Антон Степанок. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
