761231145d66490c7ffdf26f85203fd15b4a7322
Ilya <ipgromov@edu.hse.ru>
2020-03-18 05:10:45 +0300

    Script Invocation Test Run

--- SAY/SAY/HomeViewController.swift
+++ SAY/SAY/HomeViewController.swift
@@ -7,6 +7,12 @@
 //
 
 import UIKit
+import WavesSDK
+import WavesSDKCrypto
+import WavesSDKExtensions
+import Foundation
+//import XCTest
+import RxSwift
 
 struct MessageItem {
     let label: String
@@ -15,9 +21,24 @@ struct MessageItem {
 
 class HomeViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
+    public let disposeBag: DisposeBag = DisposeBag()
+    private var wavesApiService = WavesApiService()
+    private var userId = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        loadMessages()
+        
+        let crypto = WavesCrypto()
+
+        // generating the seed-phrase
+//        let newSeed: Seed = crypto.randomSeed()
+        let newSeed = "student draft picnic pass security short cook usual below prefer fashion curious scissors over opera"
+        let address: String = crypto.address(seed: newSeed, chainId: "T")!
+//        print(address)
+        guard let senderPublicKey = WavesCrypto.shared.publicKey(seed: newSeed) else { return }
+        
+        testInvokeTx(seed: newSeed, publicKey: senderPublicKey)
 
         tableView.dataSource = self
         let cellNib = UINib(nibName: "CustomTableViewCell", bundle: nil)
@@ -35,6 +56,26 @@ class HomeViewController: UIViewController, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
          return data.count
+    }
+    
+    private func loadMessages() {
+        // importing WavesCrypto part of SDK
+
+        // creating a WavesCrypto object
+        let crypto = WavesCrypto()
+
+        // generating the seed-phrase
+        let newSeed: Seed = crypto.randomSeed()
+        // getting address of this seed-phrase
+        // provide chosen network byte in the chainId field ("T" for the Testent, "W" for the Mainnet)
+        let address: String = crypto.address(seed: newSeed, chainId: "T")!
+        
+//        let test = 1
+//        wavesApiService.downloadMessages() { [weak self] block in
+//        guard let block = block else { return }
+//
+//            let newVar = block
+//        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -60,5 +101,40 @@ class HomeViewController: UIViewController, UITableViewDataSource {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func testInvokeTx(seed: Seed, publicKey: String) {
+
+        let timestamp = Int64(Date().timeIntervalSince1970) * 1000
+
+        let mid = NodeService.Query.Transaction.InvokeScript.Arg.init(value: .string("3"))
+        let text = NodeService.Query.Transaction.InvokeScript.Arg.init(value: .string("Two assure edward whence the was. Who worthy yet ten boy denote wonder. Weeks views her sight old tears sorry. Additions can suspected its concealed put furnished. Met the why particular devonshire decisively considered partiality. Certain it waiting no entered is."))
+
+        var queryModel = NodeService.Query.Transaction.InvokeScript.init(chainId: "T",
+                                                                         fee: 500000,
+                                                                         timestamp: timestamp,
+                                                                         senderPublicKey: publicKey,
+                                                                         feeAssetId: "WAVES",
+                                                                         dApp: "3N9uzN2kjrwCnrmHAhw7m3FdKhkJ69SGL6F",
+                                                                         call: .init(function: "say",args: [mid, text]),
+                                                                         payment: [.init(amount: 1, assetId: "WAVES")])
+
+        queryModel.sign(seed: seed)
+        
+        print(publicKey)
+        print(queryModel)
+        
+        let send = NodeService.Query.Transaction.invokeScript(queryModel)
+        
+        WavesSDK.shared.services
+            .nodeServices
+            .transactionNodeService
+            .transactions(query: send)
+            .observeOn(MainScheduler.asyncInstance)
+            .subscribe(onNext: { (tx) in
+                print(tx)
+            })
+            .disposed(by: disposeBag)
+
+    }
 
 }
--- SAY/SAY/WavesApiService.swift
+++ SAY/SAY/WavesApiService.swift
@@ -9,7 +9,21 @@ import WavesSDKCrypto
 
 final class WavesApiService {
 
-    let baseURL = URL(string: "")
+    let address = URL(string: "https://server.vlzhr.top/sayit/")
     let jsonDecoder = JSONDecoder()
+    
+    func downloadMessages(completion: @escaping ([Block]?) -> Void) {
+        guard let url = address else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            guard let data = data else { return }
+            let messageList = try? self?.jsonDecoder.decode([Block].self, from: data)
+            let returnData = String(data: data, encoding: .utf8)
+            completion(messageList)
+        }
+        task.resume()
+        
+        
+    }
     
 }
